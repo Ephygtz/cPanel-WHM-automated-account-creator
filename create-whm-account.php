e1a7335be81d76e09b89d761a144b8a7e71e1ca6
Ephraim <ephygtz@gmail.com>
2016-05-27 16:18:08 +0300

    Create input form

--- create-whm-account.php
+++ create-whm-account.php
@@ -53,4 +53,35 @@ if (!empty($user_name)) {
   echo "RESULT: " . $result;
 }
 
+// otherwise show input form
+else {
+$frm = <<<EOD
+<html>
+<head>
+  <title>cPanel/WHM Account Creator</title>
+  <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+  <META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE">
+</head>
+<body>
+  <style>
+    input { border: 1px solid black; }
+  </style>
+<form method="post">
+<h3>cPanel/WHM Account Creator</h3>
+<table border="0">
+<tr><td>Domain:</td><td><input name="domain" size="30"></td><td>Subdomain or domain, without www</td></tr>
+<tr><td>Username:</td><td><input name="user" size="30"></td><td>Username to be created</td></tr>
+<tr><td>Password:</td><td><input name="password" size="30"></td><td></td></tr>
+<tr><td>Package:</td><td><input name="package" size="30"></td><td>Package (hosting plan) name. Make sure you specify existing package</td></tr>
+<tr><td>Contact Email:</td><td><input name="email" size="30"></td><td></td></tr>
+<tr><td colspan="3"><br /><input type="submit" value="Create Account"></td></tr>
+</table>
+</form>
+</body>
+</html>
+EOD;
+echo $frm;
+}
+
+
  ?>
