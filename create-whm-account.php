892de50855b34b720aa323cd71c25f173512b186
Ephraim <ephygtz@gmail.com>
2018-04-09 08:52:19 +0300

    Push updates

--- create-whm-account.php
+++ create-whm-account.php
@@ -1,78 +1,74 @@
 
 <?php
 
-// YOUR WHM LOGIN DATA, CHANGE AS APPROPRIATE
+		// YOUR WHM LOGIN DATA, CHANGE AS APPROPRIATE
 
-$whm_user = "root"; // reseller username
-$whm_pass = "password"; // the password you use to login to WHM
+		$whm_user = "root"; // reseller username
+		$whm_pass = "password"; // the password you use to login to WHM
 
-// DO NOT EDIT BELOW THIS
+		// DO NOT EDIT BELOW THIS
 
-$whm_host = $_SERVER['HTTP_HOST'];
+		$whm_host = $_SERVER['HTTP_HOST'];
 
-function getVar($name, $def = '')
-	{
-	if (isset($_REQUEST[$name])) return $_REQUEST[$name];
-	  else return $def;
-	}
+		function getVar($name, $def = ''){
+			if (isset($_REQUEST[$name])){
+				return $_REQUEST[$name];
+			} else{
+				return $def;
+			} 
+		}
 
-// Domain name of new hosting account
-// To create subdomain just pass full subdomain name
-// Example: newuser.jabalisites.com
+		// Domain name of new hosting account
+		// To create subdomain just pass full subdomain name
+		// Example: newuser.jabalisites.com
 
-if (!isset($user_domain))
-	{
-	$user_domain = getVar('domain');
-	}
+		if (!isset($user_domain)){
+			$user_domain = getVar('domain');
+		}
 
-// Username of the new hosting account
+		// Username of the new hosting account
 
-if (!isset($user_name))
-	{
-	$user_name = getVar('user');
-	}
+		if (!isset($user_name)){
+			$user_name = getVar('user');
+		}
 
-// Password for the new hosting account
+		// Password for the new hosting account
 
-if (!isset($user_pass))
-	{
-	$user_pass = getVar('password');
-	}
+		if (!isset($user_pass)){
+			$user_pass = getVar('password');
+		}
 
-// New hosting account Package
+		// New hosting account Package
 
-if (!isset($user_plan))
-	{
-	$user_plan = getVar('package');
-	}
+		if (!isset($user_plan)){
+			$user_plan = getVar('package');
+		}
 
-// Contact email
+		// Contact email
 
-if (!isset($user_email))
-	{
-	$user_email = getVar('email');
-	}
+		if (!isset($user_email)){
+			$user_email = getVar('email');
+		}
 
-// if parameters passed then create account
+		// if parameters passed then create account
 
-if (!empty($user_name))
-	{
+		if (!empty($user_name)){
 
-	// create account on the cPanel server
+			// create account on the cPanel server
 
-	$script = "http://{$whm_user}:{$whm_pass}@{$whm_host}:2086/scripts/wwwacct";
-	$params = "?plan={$user_plan}&domain={$user_domain}&username={$user_name}&password={$user_pass}&contactemail={$user_email}";
-	$result = file_get_contents($script . $params);
+			$script = "http://{$whm_user}:{$whm_pass}@{$whm_host}:2086/scripts/www";
+			$params = "?plan={$user_plan}&domain={$user_domain}&username={$user_name}&password={$user_pass}&contactemail={$user_email}";
+			$result = file_get_contents($script . $params);
 
-	// output result
+			// output result
 
-	echo "RESULT: " . $result;
-	}
+			echo "RESULT: " . $result;
+		}
 
-// otherwise show input form
+		// otherwise show input form
 
-  else
-	{
+		// 
+		else {
 	$frm = <<<EOD
 <html>
 <head>
